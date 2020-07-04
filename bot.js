ef6be6c71c99d8ac7a97aabcd4b9b0f53ef4d5a0
Olivia Cobble <ocobble@hotmail.com>
2020-07-04 15:09:34 -0500

    Added first draft of tweet code

--- bot.js
+++ bot.js
@@ -2,3 +2,21 @@ var twit = require(’twit’);
 var config = require(’./config.js’);
 
 var Twitter = new twit(config);
+
+var retweet = function() {
+    var params = {
+        q: '#nodejs, #Nodejs',  // REQUIRED
+        lang: 'en'
+}
+
+Twitter.post('statuses/update', {
+                status: 'happy birthday!'
+            }, function(err, data, response) {
+                if (response) {
+                    console.log('Tweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
