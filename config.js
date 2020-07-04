cc394fc4dec0ada0684f503a8fbe259fd048eb06
Olivia Cobble <ocobble@hotmail.com>
2020-07-04 14:45:46 -0500

    Added Twitter credentials to configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'jvKPzS3P3MznvfhbL7NBWd63Z',  
+  consumer_secret: 'NxRe21IFZyrmjPbi3toFwiwHHDJYhgqtZih8TUSLmDPfM42i2N',
+  access_token: '824067883764424705-zBt7xqpAaay97iDQdmRjIledVUcPWat',  
+  access_token_secret: 'rfrLPfr1LC3Gu7Mk3BRRKF18RVpIwxtPF9CTWgQ813MA3'
+}
