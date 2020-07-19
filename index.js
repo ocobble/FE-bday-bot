4bcd6f3b0cc7589af62e6878578d3bd379f392d2
Olivia Cobble <ocobble@hotmail.com>
2020-07-18 20:11:14 -0500

    Added an index file (??)

---
+++ index.js
@@ -0,0 +1,32 @@
+var http, director, cool, bot, router, server, port;
+
+http        = require('http');
+director    = require('director');
+cool        = require('cool-ascii-faces');
+bot         = require('./bot.js');
+
+router = new director.http.Router({
+  '/' : {
+    get: ping
+  }
+});
+
+server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
+
+  router.dispatch(req, res, function(err) {
+    res.writeHead(err.status, {"Content-Type": "text/plain"});
+    res.end(err.message);
+  });
+});
+
+port = Number(process.env.PORT || 5000);
+server.listen(port);
+
+function ping() {
+  this.res.end("Hello from FE birthday bot");
+  bot.tweetBirthdays();
+}
