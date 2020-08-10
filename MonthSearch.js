7a4becb818ae6dfbc616a1f74315364389c8004f
Olivia Cobble <ocobble@hotmail.com>
2020-08-10 15:00:12 -0500

    Implemented month search

---
+++ MonthSearch.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+function monthSearch(month) {
+    // Find out which characters have birthdays this month:
+    var text = fs.readFileSync("./Characters.json", "utf-8");
+    var jsonText = JSON.parse(text);
+
+    console.log("Birthdays this month: ");
+    for (var i = 0; i < jsonText.Characters.length; ++i) {
+        var characterBD = new Date(jsonText.Characters[i].Birthday);
+        if (characterBD.getMonth() == month) {
+            console.log(jsonText.Characters[i].Name + ": "  + characterBD.getDate());
+        }
+    }
+}
+
+monthSearch(7);
+//exports.monthSearch = monthSearch;
