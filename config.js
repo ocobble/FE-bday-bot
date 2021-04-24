b21cacbd1612f8b50c66afbdab4594424a257b99
Olivia Cobble <ocobble@hotmail.com>
2021-04-24 10:29:09 -0500

    Added extra security

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  consumer_key: 'Q9ZmH4kRNzFdGdo2E4bXxaesx',  
-  consumer_secret: 'ckUANXggoPjZFWaaRWLnGLLSFNjOWmTtPLV6z1AB03927LAPxg',
-  access_token: '1279511100225191936-64VXyuVlXNciPnKguc9uRRa8bGQhFy',  
-  access_token_secret: '938SDoWLGK0tco9ycce0cPoo7GqMjgXmvBng2ojZyRKaM'
+  consumer_key: process.env.CONSUMER_KEY,  
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,  
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
-
-// API Key: Ptr8Q5X8lTDoMesW8NR3UIuUl
-// API key secret: QCSqMkzIB4dOo1ZB1e2N60t2gDXYnFQtJVsfs3ziLBCxNxm6iU
-// Bearer token: AAAAAAAAAAAAAAAAAAAAABbOFgEAAAAA2c9hzJH6MGGK8J1FeDHx7j%2FYqKY%3DnWZcvdDVDE84aYr8D4GxChPKZ59bGbXBZVvw4W7tAEG8Z1StMj
